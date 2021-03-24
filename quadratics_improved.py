ffac01099d0c32226a100004635367843c3a319d
Daniilvolodin <51727111+Daniilvolodin@users.noreply.github.com>
2021-03-24 18:30:48 +1300

    Update quadratics_improved.py

--- quadratics_improved.py
+++ quadratics_improved.py
@@ -11,34 +11,49 @@ class Algebra_Entry:
         # Acceptable array prevents number zero from
         # appearing in question
         acceptable = [random.randrange(-9, -1), random.randrange(1, 9)]
-        
+
         # Generates two integer variables from the range between -9,-1
         # and 1,9. One integer per variable
         random1, random2 = random.choice(acceptable), random.choice(acceptable)
-        
+
         # Generates two co-efficients between 1,3
         coef_pick = [1, 2, 3]
         x_coefficient1 = random.choice(coef_pick)
         x_coefficient2 = random.choice(coef_pick)
-        
+        operators = ['+', '-']
         show1, show2 = random1, random2
-
 
         if random1 >= 0:
             show1 = "+" + str(random1)
         if random2 >= 0:
             show2 = "+" + str(random2)
 
-        question = "({}{})({}{})".format(x_coefficient1, show1, x_coefficient2, show2)
+        question = "({}x{})({}x{})".format(x_coefficient1, show1, x_coefficient2, show2)
 
         a_value = x_coefficient1 * x_coefficient2
         b_value = (x_coefficient1 * random2) + (x_coefficient2 * random1)
         c_value = random1 * random2
+        b_op = b_value
+        c_op = c_value
+
+        if b_value >= 0:
+            b_op = "+" + str(b_value)
+        if c_value >= 0:
+            c_op = "+" + str(c_value)
 
         answer_dict = {
-            "Correct": "{}x²{}x{}".format(a_value, b_value, c_value)
-        }
+            "Correct": "{}x²{}x{}".format(a_value, b_op, c_op),
+            "Incorrect1": "{}x²{}x{}".format(a_value, str(b_op).replace(str(b_op)[0],
+                                             str(operators[operators.index(str(b_op)[0])-1])), c_op),
 
+            "Incorrect2": "{}x²{}x{}".format(a_value, b_op, random1 + random2),
+            "Incorrect3": "{}x²{}x{}".format(a_value, int(b_op)+random2, c_op)
+        }
+        print(int(b_op)+random2)
+        print(answer_dict.get("Incorrect1"))
+        print(answer_dict.get("Incorrect2"))
+        print(answer_dict.get("Incorrect3"))
+        print(answer_dict.get("Correct"))
         self.parameter = parameter
         self.option_value = IntVar()
 
@@ -83,6 +98,7 @@ class Algebra_Entry:
     def get_value(self):
         q = self.option_value.get()
         print(q)
+
 
 # Initialise GUI window
 root = Tk()
